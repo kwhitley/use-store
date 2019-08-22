25e298aa62e1939e91cb2ed14968239bf37dd551
Kevin Whitley <krwhitley@gmail.com>
2019-08-22 16:02:54 -0500

    released v1.5.2 - adds test coverage of magicSetter behavior

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
 import { renderHook, cleanup, act, unmount } from '@testing-library/react-hooks'
 
 // test files
@@ -5,6 +7,13 @@ import { useStore } from '../src/index.js'
 
 const VALUE = 0
 const SETTER = 1
+
+const createInput = ([ value, onChange ]) => {
+  const utils = render(<input aria-label="mock-input" value={value} onChange={onChange} />)
+  const input = utils.getByLabelText('mock-input')
+
+  return input
+}
 
 describe('@kwhitley/use-store', () => {
   describe('useStore(namespace:string, initialValue:anything, options:object) : function', () => {
@@ -42,6 +51,31 @@ describe('@kwhitley/use-store', () => {
       const { result } = renderHook(() => useStore('test2', 1, { persist: true })) // initialize to 1
 
       expect(result.current[VALUE]).toBe(1) // but expect 2 from previous test
+    })
+
+    describe('magicSetter behavior (automatic derivation of Event/SyntheticEvents values)', () => {
+      test('setValue can be used directly with onChange (without event.target.value)', () => {
+        const { result } = renderHook(() => useStore('test-input', 1))
+
+        act(() => {
+          let input = createInput(result.current)
+          fireEvent.change(input, { target: { value: '23' } })
+        })
+
+        expect(result.current[VALUE]).toBe('23')
+      })
+
+      test('setting value to object matching event signature will not infer value', () => {
+        const { result } = renderHook(() => useStore('test-input', 1))
+
+        const fakeEvent = { target: { value: 3 }}
+
+        act(() => {
+          act(() => result.current[SETTER](fakeEvent))
+        })
+
+        expect(result.current[VALUE]).toBe(fakeEvent)
+      })
     })
   })
 })
