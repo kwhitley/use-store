1f948872e7ee6526fcf02620037c86333392ae34
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-08-24 13:01:34 -0500

    debounce message handler and don't update for messages coming from self

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import localstorify from 'localstorify'
 // prefix for localstorify
 const GLOBALSTORAGE_PREFIX = '!ush::'
 
+const debounce = (func, delay = 100) => { 
+  let timer 
+  return function() { 
+    const context = this
+    const args = arguments 
+    clearTimeout(timer) 
+    timer = setTimeout(() => func.apply(context, args), delay) 
+  } 
+} 
+
 // individual Store implementation for tracking values/setters
 export class Store {
   constructor({ value, namespace, options }) {
     this.state = value
+    this.id = String(Math.floor(Math.random() * 10000))
 
     if (options.persist) {
       try {
@@ -22,7 +33,7 @@ export class Store {
 
     if (options.broadcast && window.BroadcastChannel) {
       this.channel = new BroadcastChannel(GLOBALSTORAGE_PREFIX + namespace)
-      this.channel.addEventListener('message', this.handleMessage)
+      this.channel.addEventListener('message', debounce(this.handleMessage, 300))
     }
 
     this.options = options
@@ -31,21 +42,21 @@ export class Store {
   }
 
   handleMessage = (e) => {
-    if (!e.data || e.data === this.state) {
+    if (!e.data || e.data.id === this.id) {
       return
     }
-    this.setState(e.data)
+    this.setState(e.data.message, { broadcast: false })
   }
 
-  setState = (value) => {
+  setState = (value, options = { broadcast: true }) => {
     this.state = value
     if (this.options.persist) {
       localstorify.setItem(GLOBALSTORAGE_PREFIX + this.namespace, JSON.stringify(value))
     }
-    if (this.options.broadcast && this.channel) {
-      this.channel.postMessage(value)
-    }
     this.setters.forEach(setter => setter(this.state))
+    if (options.broadcast) {
+      this.channel.postMessage({ id: this.id, message: value })
+    }
   }
 }
 
