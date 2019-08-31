84abb550dafdc1e053fa7128f40cc03c51cd646d
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-08-30 20:41:13 -0500

    use uuid generate function instead of raw Math.random

--- src/index.js
+++ src/index.js
@@ -14,11 +14,19 @@ const debounce = (func, delay = 100) => {
   } 
 } 
 
+// https://stackoverflow.com/a/2117523/11599918
+const uuid = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 // individual Store implementation for tracking values/setters
 export class Store {
   constructor({ value, namespace, options }) {
     this.state = value
-    this.id = String(Math.floor(Math.random() * 10000))
+    this.id = uuid()
 
     if (options.persist) {
       try {
